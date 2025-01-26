858aea9a803f7c5c370ec55fb0a2e293a24bb026
Mohsin-Creater <ranamohsin11874@gmail.com>
2025-01-26 06:56:03 +0500

    ADDING LIBRARIES AND WELCOME MSG

---
+++ HMS.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<conio.h>
+#include<time.h>
+#include<iomanip>
+
+using namespace std;
+int main()
+{
+char fname[20];
+time_t rawtime;
+struct tm * timeinfo;
+
+time ( &rawtime );
+timeinfo = localtime ( &rawtime );
+
+re:
+cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+cout<<"\t\t\t\t\t@@ _______________________________________________________________________________________ @@\n";
+cout<<"\t\t\t\t\t@@|                                           		                                  |@@\n";
+cout<<"\t\t\t\t\t@@|                                           		                                  |@@\n";
+cout<<"\t\t\t\t\t@@|                                           		                                  |@@\n";
+cout<<"\t\t\t\t\t@@|                                           		                                  |@@\n";
+cout<<"\t\t\t\t\t@@|                                           		                                  |@@\n";
+cout<<"\t\t\t\t\t@@|                                           		                                  |@@\n";
+cout<<"\t\t\t\t\t@@|                                  WELCOME TO                                           |@@\n";
+cout<<"\t\t\t\t\t@@|                                                                                       |@@\n";
+cout<<"\t\t\t\t\t@@|                           HOSPITAL MANAGEMENT SYSTEM                                  |@@\n";
+cout<<"\t\t\t\t\t@@|                                                                                       |@@\n";
+cout<<"\t\t\t\t\t@@|                  	   YOUR HEALTH IS OUR FIRST PRIORITY                              |@@\n";
+cout<<"\t\t\t\t\t@@|                                                                                       |@@\n";
+cout<<"\t\t\t\t\t@@|                                                                                       |@@\n";
+cout<<"\t\t\t\t\t@@|                                                                                       |@@\n";
+cout<<"\t\t\t\t\t@@|                                                                                       |@@\n";
+cout<<"\t\t\t\t\t@@|                                                                                       |@@\n";
+cout<<"\t\t\t\t\t@@|                                                -Brought To You by MR, AELIYA, RAFFAY  |@@\n";
+cout<<"\t\t\t\t\t@@|_______________________________________________________________________________________|@@\n";
+cout<<"\t\t\t\t\t@@                                                                                         @@\n";
+cout<<"\t\t\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n\n\n\t\t\t\t\t";
+system("pause");
+system("cls");
+
+
+
