c9dcf2006e093daffc7291834717b9c395837c61
Mohsin-Creater <ranamohsin11874@gmail.com>
2025-01-26 07:00:48 +0500

    Choices Added

--- HMS.cpp
+++ HMS.cpp
@@ -40,6 +40,38 @@ cout<<"\t\t\t\t\t@@                                                             
 cout<<"\t\t\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n\n\n\t\t\t\t\t";
 system("pause");
 system("cls");
+int i;
+int login();
+login();
 
+//giving option to the user for their choice
+b:
+cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t  HOSPITAL MANAGEMENT SYSTEM \n\n";	
+cout<<"\n\n\t\t\t\t\t\tPlease,  Choose from the following Options: \n\n";
+cout<<"\t\t\t\t\t\t _________________________________________________________________ \n";
+cout<<"\t\t\t\t\t\t|                                           	                  |\n";
+cout<<"\t\t\t\t\t\t|                                                                 |\n";
+cout<<"\t\t\t\t\t\t|             1  >> Add New Patient Record                        |\n";
+cout<<"\t\t\t\t\t\t|                                                                 |\n";
+cout<<"\t\t\t\t\t\t|             2  >> Add Diagnosis Information                     |\n";
+cout<<"\t\t\t\t\t\t|                                                                 |\n";
+cout<<"\t\t\t\t\t\t|             3  >> Full History of the Patient                   |\n";
+cout<<"\t\t\t\t\t\t|                                                                 |\n";
+cout<<"\t\t\t\t\t\t|             4  >> Information About the Hospital                |\n";
+cout<<"\t\t\t\t\t\t|                                                                 |\n";
+cout<<"\t\t\t\t\t\t|             5  >> Exit the Program                              |\n";
+cout<<"\t\t\t\t\t\t|                                                                 |\n";
+cout<<"\t\t\t\t\t\t|_________________________________________________________________|\n\n";
+
+a:cout<<"\t\t\t\t\t\tEnter your choice: ";cin>>i;
+
+if(i>5||i<1)
+{
+cout<<"\n\n\t\t\t\t\t\tInvalid Choice\n";cout<<"\t\t\t\t\t\tTry again...........\n\n";goto a;
+} //if inputed choice is other than given choice
+
+
+
+system("cls");
 
 
